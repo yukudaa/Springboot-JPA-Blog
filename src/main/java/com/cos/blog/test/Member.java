e8cf6fb250e05cc855f02344de306121aa4e5a1a
yukuda <leeyookoo@naver.com>
2021-07-30 15:42:20 +0900

    Member

--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,16 +1,20 @@
 package com.cos.blog.test;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+//@Getter
+//Setter
+@Data   // getter + setter
+//@AllArgsConstructor // 전체 constructor
+@NoArgsConstructor // 빈 constructor
+
 public class Member {
     private int id;
     private String username;
     private String password;
     private String email;
 
+    @Builder
     public Member(int id, String username, String password, String email) {
         this.id = id;
         this.username = username;
