08e26580012317003afb8448fc4b3eebba646f3b
yukuda <leeyookoo@naver.com>
2021-07-30 15:42:15 +0900

    httpController

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -9,10 +9,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HttpControllerTest {
 
+    private static final String TAG = "HttpControllerTest: ";
+
+    @GetMapping("/http/lombok")
+    public String lombokTest() {
+        //Member m = new Member(1,"ssar","1234","email");
+        Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+        System.out.println(TAG +"getter:"+m.getUsername());
+        m.setUsername("cos");
+        System.out.println(TAG +"setter:"+m.getUsername());
+        return "lombok test 완료";
+    }
+
     // 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
     // http://localhost:8080/http/get (select)
     @GetMapping("/http/get")
     public String getTest(Member m) {
+
         return "get 요청 : "+m.getId()+","+m.getUsername()+","+m.getPassword()+","+m.getEmail();
     }
 
