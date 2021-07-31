ddcd652f2b0d0dc0a25db796f389bb599d80b6d6
yukuda <leeyookoo@naver.com>
2021-07-31 16:30:05 +0900

    Board

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,32 @@
+package com.cos.blog.model;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob // 대용량 데이터
+    private String content; // 섬머노트 라이브러리 <html>태그가 섞여서 디자인이 됨.
+
+    @ColumnDefault("0")
+    private int count; // 조회수
+
+    @ManyToOne // Board = Many, User = One
+    @JoinColumn(name="userId") // userId라는 이름으로 조인이 된다.
+    private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+
+    @CreationTimestamp // 데이터가 인서트 또는 업데이트될때 자동으로 현재시간이 들어감
+    private Timestamp createDate;
+
+
+}
