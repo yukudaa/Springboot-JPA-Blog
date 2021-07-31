fef8a0b532293c5a179241fb3cfe2c2da0de9ab9
yukuda <leeyookoo@naver.com>
2021-07-31 21:05:08 +0900

    User

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -1,5 +1,9 @@
 package com.cos.blog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -7,6 +11,10 @@ import java.sql.Timestamp;
 
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // 빌더 패턴!!
 // ORM -> Jave(다른언어) Object -> 테이블로 매핑해주는 기술
 @Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
 public class User {
