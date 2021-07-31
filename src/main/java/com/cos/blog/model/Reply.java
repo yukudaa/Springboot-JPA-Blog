c153648c2dc8af6d56e338de400c34eba552d730
yukuda <leeyookoo@naver.com>
2021-07-31 21:05:04 +0900

    Reply

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -1,4 +1,36 @@
 package com.cos.blog.model;
 
-public class Reply {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Reply {    // 답변
+
+    @Id //Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다
+    private int id; // 시퀀스, auto_increment
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name="boardId") // 필드명
+    private Board board;    // 게시판
+
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User user; // 답변을 누가 적었는지
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
