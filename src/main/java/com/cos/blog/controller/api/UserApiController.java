090991c15ec848b9c8c65c6f5836d0c9f70fcf05
yukuda <leeyookoo@naver.com>
2021-08-10 18:16:17 +0900

    UserApiController

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,7 +1,10 @@
 package com.cos.blog.controller.api;
 
 import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
+import com.cos.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserApiController {
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/api/user")
-    public ResponseDto<Integer> save(@RequestBody User user){
+    public ResponseDto<Integer> save(@RequestBody User user){ // username, password, email
         System.out.println("UserApiController : save 호출됨");
         // 실제로 DB에 insert를 하고 아래에서 return이 되면 되요.
-        return new ResponseDto<Integer>(HttpStatus.OK,1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+        user.setRole(RoleType.USER);
+        int result = userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),result); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
     }
 }
