bf256395ad2051fbb245eb949b1c4d8ca63e4a09
yukuda <leeyookoo@naver.com>
2021-08-10 18:16:56 +0900

    UserService

---
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,28 @@
+package com.cos.blog.service;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+// 스프링이 커포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다.
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public int 회원가입(User user) {
+        try {
+            userRepository.save(user);
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("UserService : 회원가입() :" + e.getMessage());
+        }
+        return -1;
+    }
+}
