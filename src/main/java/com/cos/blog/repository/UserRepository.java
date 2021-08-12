3f3ef317774e0601c6557fc5ba671e9f3b8ebf19
yukuda <leeyookoo@naver.com>
2021-08-12 16:18:03 +0900

    UserRepository

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.cos.blog.repository;
 
 import com.cos.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 // DAO
 // 자동으로 bean등록이 된다.
@@ -9,3 +10,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User,Integer> {
 
 }
+
+
+// JPA Naming 전략
+// SELECT * FROM user WHERE username = ? AND password = ?;
+// User findByUsernameAndPassword(String username,String password);
+
+// @Query(value = "SELECT * FROM user WHERE username = ? AND password = ?",nativeQuery = true)
+// User login(String username, String password);
