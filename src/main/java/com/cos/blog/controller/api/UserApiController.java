6f9e76a25be27ec28be85f698e8a1a5c7cfdc705
yukuda <leeyookoo@naver.com>
2021-08-12 16:17:55 +0900

    UserApiController

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -10,18 +10,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class UserApiController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/api/user")
+    @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user){ // username, password, email
         System.out.println("UserApiController : save 호출됨");
         // 실제로 DB에 insert를 하고 아래에서 return이 되면 되요.
         user.setRole(RoleType.USER);
-        int result = userService.회원가입(user);
-        return new ResponseDto<Integer>(HttpStatus.OK.value(),result); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+        userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
     }
+// 전통적인 방법
+//    @PostMapping("/api/user/login")
+//    public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+//        System.out.println("UserApiController : login 호출됨");
+//        User principal = userService.로그인(user); // principal (접근주체)
+//
+//        if(principal != null) {
+//            session.setAttribute("principal",principal);
+//        }
+//        return new ResponseDto<Integer>(HttpStatus.OK.value(),1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+//    }
+
 }
