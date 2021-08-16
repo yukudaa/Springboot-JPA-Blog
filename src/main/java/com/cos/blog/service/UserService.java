968756d81d2cfcd20b14cf0511fdadddd3fa8e19
yukuda <leeyookoo@naver.com>
2021-08-16 21:32:38 +0900

    UserService

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -4,6 +4,7 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,8 +16,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     @Transactional
-    public int 회원가입(User user) {
+    public void 회원가입(User user) {
+        String rawPassword = user.getPassword(); // 1234 원문
+        String encPassword = encoder.encode(rawPassword); // 해쉬
+        user.setPassword(encPassword);
+        user.setRole(RoleType.USER);
         userRepository.save(user);
     }
 
