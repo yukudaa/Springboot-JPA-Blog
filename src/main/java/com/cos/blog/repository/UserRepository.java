7a64d1199059721243670399397142e27acfac1c
yukuda <leeyookoo@naver.com>
2021-08-16 21:32:32 +0900

    UserRepository

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -4,11 +4,14 @@ import com.cos.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 // DAO
 // 자동으로 bean등록이 된다.
 // @Repository 생략 가능하다.
 public interface UserRepository extends JpaRepository<User,Integer> {
-
+    // SELECT * FROM user WHERE username = 1?;
+    Optional<User> findByUsername(String username);
 }
 
 
