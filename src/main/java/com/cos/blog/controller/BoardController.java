1e436bf9718fda67f763da9135895a7fac56c7e8
yukuda <leeyookoo@naver.com>
2021-08-18 16:01:57 +0900

    BoardController

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,21 +1,31 @@
 package com.cos.blog.controller;
 
 import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
-//    @Autowired
-//    private PrincipalDetail principal;
 
-    @GetMapping({"","/"}) // 컨트롤로에서 세션을 어떻게 찾는지?
-    public String index(@AuthenticationPrincipal PrincipalDetail principal) {
-        // WEB-INF/views/index.jsp
-        System.out.println("로그인 사용자 아이디: "+principal.getUsername());
-        return "index";
+    @Autowired
+    private BoardService boardService;
+
+    // 컨트롤로에서 세션을 어떻게 찾는지?
+    // @AuthenticationPrincipal PrincipalDetail principal
+    @GetMapping({"","/"})
+    public String index(Model model) {
+        model.addAttribute("boards",boardService.글목록());
+        return "index"; // viewResolver 작동!
+    }
+
+    // USER 권한이 필요
+    @GetMapping("/board/saveForm")
+    public String saveForm() {
+        return "board/saveForm";
     }
 }
