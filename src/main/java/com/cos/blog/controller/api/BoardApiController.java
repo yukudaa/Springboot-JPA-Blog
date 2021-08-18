f8fc80dce9c9a1bd5e941c6222b0ad33db84efa6
yukuda <leeyookoo@naver.com>
2021-08-18 16:02:21 +0900

    BoardApiController

---
+++ src/main/java/com/cos/blog/controller/api/BoardApiController.java
@@ -0,0 +1,30 @@
+package com.cos.blog.controller.api;
+
+import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.dto.ResponseDto;
+import com.cos.blog.model.Board;
+import com.cos.blog.model.User;
+import com.cos.blog.service.BoardService;
+import com.cos.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardApiController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @PostMapping("/api/board")
+    public ResponseDto<Integer> save(@RequestBody Board board, @AuthenticationPrincipal PrincipalDetail principal){
+
+        boardService.글쓰기(board,principal.getUser());
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
+    }
+
+
+}
