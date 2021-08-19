788c8c957b3c546aee27a40876048cd096b15736
yukuda <leeyookoo@naver.com>
2021-08-19 16:00:37 +0900

    BoardController

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -3,10 +3,14 @@ package com.cos.blog.controller;
 import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -18,9 +22,21 @@ public class BoardController {
     // 컨트롤로에서 세션을 어떻게 찾는지?
     // @AuthenticationPrincipal PrincipalDetail principal
     @GetMapping({"","/"})
-    public String index(Model model) {
-        model.addAttribute("boards",boardService.글목록());
+    public String index(Model model,@PageableDefault(size=3,sort="id",direction = Sort.Direction.DESC) Pageable pageable) {
+        model.addAttribute("boards",boardService.글목록(pageable));
         return "index"; // viewResolver 작동!
+    }
+
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable int id,Model model) {
+        model.addAttribute("board",boardService.글상세보기(id));
+        return "board/detail";
+    }
+
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm(@PathVariable int id, Model model) {
+        model.addAttribute("board",boardService.글상세보기(id));
+        return "board/updateForm";
     }
 
     // USER 권한이 필요
