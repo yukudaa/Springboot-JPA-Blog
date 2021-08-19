11fcf0d5a83c0c9d42963a7af38dfe61c0980bf6
yukuda <leeyookoo@naver.com>
2021-08-19 16:00:53 +0900

    BoardService

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -6,6 +6,8 @@ import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,13 +23,41 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     @Transactional
-    public void 글쓰기(Board board,User user) { // title, content
+    public void 글쓰기(Board board, User user) { // title, content
         board.setContent("0");
         board.setUser(user);
         boardRepository.save(board);
     }
 
-    public List<Board> 글목록(){
-        return boardRepository.findAll();
+    @Transactional(readOnly = true)
+    public Page<Board> 글목록(Pageable pageable) {
+        return boardRepository.findAll(pageable);
     }
+
+    @Transactional(readOnly = true)
+    public Board 글상세보기(int id) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> {
+                    return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
+                });
+    }
+
+    @Transactional
+    public void 글삭제하기(int id) {
+        System.out.println("글삭제하기:" + id);
+        boardRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void 글수정하기(int id, Board requsetBoard) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> {
+                    return new IllegalArgumentException("글 찾기 실패 : 아이디를 찾을 수 없습니다.");
+                }); //영속화 완료
+        board.setTitle(requsetBoard.getTitle());
+        board.setContent(requsetBoard.getContent());
+        // 해당 함수로 종료시(Service가 종료될 때) 트랜잭션이 종료됩니다. 이때 더티체킹 - 자동 업데이트가 됨. db flush
+    }
+
+
 }
