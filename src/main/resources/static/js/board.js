793b70c5776ff31666b5b3c41724c180190994a2
yukuda <leeyookoo@naver.com>
2021-08-19 16:01:17 +0900

    board.js

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,9 +3,12 @@ let index = {
         $("#btn-save").on("click", ()=>{    // function(){}, ()=>{} this를 바인딩하기 위해서!!
             this.save();
         });
-        // $("#btn-login").on("click", ()=>{    // function(){}, ()=>{} this를 바인딩하기 위해서!!
-        //     this.login();
-        // });
+        $("#btn-delete").on("click", ()=>{    // function(){}, ()=>{} this를 바인딩하기 위해서!!
+            this.deleteById();
+        });
+        $("#btn-update").on("click", ()=>{    // function(){}, ()=>{} this를 바인딩하기 위해서!!
+            this.update();
+        });
     },
 
     save:function(){
@@ -30,7 +33,44 @@ let index = {
         }); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
     },
 
+    deleteById:function(){
+        let id = $("#id").text();
 
+        $.ajax({
+            // 회원가입 수행 요청
+            type: "DELETE",
+            url: "/api/board"+id,
+           dataType: "json" // 요청을 서버로해서 응답이 왔을때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript오브젝트로 변경
+        }).done(function(resp){
+            alert("삭제가 완료되었습니다.");
+            location.href= "/";
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        }); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
+    },
+
+    update:function(){
+        let id = $("#id").val();
+
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val(),
+        };
+
+        $.ajax({
+            // 회원가입 수행 요청
+            type: "PUT",
+            url: "/api/board"+id,
+            data: JSON.stringify(data), // http body데이터
+            contentType: "application/json; charset=utf-8", // body데이터가 어떤 타입인지(MIME)
+            dataType: "json" // 요청을 서버로해서 응답이 왔을때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript오브젝트로 변경
+        }).done(function(resp){
+            alert("글수정이 완료되었습니다.");
+            location.href= "/";
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        }); // ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청!!
+    },
 }
 
 index.init();
