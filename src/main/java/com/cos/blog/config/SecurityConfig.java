dfad16e69b29fc35940b63c5aa3bc6fde3abe91c
yukuda <leeyookoo@naver.com>
2021-08-19 16:00:28 +0900

    SecurityConfig

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable() // csrf 토큰 비활성화 (테스트시 걸어두는게 좋음)
                 .authorizeRequests()
-                    .antMatchers("/","/auth/**","/js/**","/css/**","/image/**")
+                    .antMatchers("/","/auth/**","/js/**","/css/**","/image/**","/dummy/**")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
