ee4e75619c182fb58737addaea13cf3019c10304
yukuda <leeyookoo@naver.com>
2021-08-23 23:49:45 +0900

    UserApiController

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +40,9 @@ public class UserApiController {
 //        return new ResponseDto<Integer>(HttpStatus.OK.value(),1); // 자바오브젝트를 JSON으로 변환해서 리턴 (Jackson)
 //    }
 
+    @PutMapping("/user")
+    public ResponseDto<Integer> update(@RequestBody User user){ // Json 데이터 받고 싶으면 @RequestBody적음
+        userService.회원수정(user);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+    }
 }
