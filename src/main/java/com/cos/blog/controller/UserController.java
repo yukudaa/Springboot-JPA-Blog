3c9e4aa5854e7091a0545f9e97d8c0cc669792c3
yukuda <leeyookoo@naver.com>
2021-08-24 16:12:01 +0900

    UserController

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.cos.blog.controller;
 
+import com.cos.blog.config.auth.PrincipalDetail;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 // 인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 허용
 // 그냥 주소가 / 이면 index.jsp 허용
@@ -22,7 +26,7 @@ public class UserController {
     }
 
     @GetMapping("/user/updateForm")
-    public String updateForm() {
+    public String updateForm(@AuthenticationPrincipal PrincipalDetail principal) {
         return "user/updateForm";
     }
 
