349e53815984d996b6c85feea1a065ab1744b568
yukuda <leeyookoo@naver.com>
2021-08-24 16:12:11 +0900

    UserApiController

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,11 +1,17 @@
 package com.cos.blog.controller.api;
 
+import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -41,8 +47,15 @@ public class UserApiController {
 //    }
 
     @PutMapping("/user")
-    public ResponseDto<Integer> update(@RequestBody User user){ // Json 데이터 받고 싶으면 @RequestBody적음
+    public ResponseDto<Integer> update(@RequestBody User user, @AuthenticationPrincipal PrincipalDetail principal, HttpSession session){ // Json 데이터 받고 싶으면 @RequestBody적음
         userService.회원수정(user);
+        // 여기서는 트랜잭션이 종료되기 때문에 DB에 값은 변경이 됐음.
+        // 하지만 세션값은 변경되지 않은 상태이기 때문에 우리가 직접 세션값을 변경해줄 것임.
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(principal,null);
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+        session.setAttribute("SPRING_SECURITY_CONTEXT",securityContext);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
     }
 }
